cc4c41e568328d863dda6cfe9163e1d352cbf2d6
Chester Tan <chesterlbtan@gmail.com>
2019-03-31 21:29:43 +0800

    Add error handling for the website

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')\
                               or "postgres://chester:hello987@192.168.0.50:5432/anghari_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # --- FOR EMAIL ERROR SENDING ---
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # ADMINS = ['your-email@example.com']
